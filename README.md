9d04ecab366418dbad991d7193ee126750480262
Olalekan22385 <oyelamiabdulqudus@gmail.com>
2024-10-16 10:52:05 +0100

    Library commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This tasks is about modelling a library, writing functions to; view list of books, add new books, delet last book added, and get out any book
---
+++ libra.js
@@ -0,0 +1,36 @@
+// An aaray of books repping a library
+const library = ['Rich dad Poor dad', ' Gifted Hands',
+' Atomic Habits', ' Business Intelligence for Dummies', 
+' Don\'t give a fuck', ' Psychology of money',
+ ' How to talk to any one', ' Mindset', 
+ ' How to be a Stoic', ' Think and Grow Rich' 
+]
+
+console.log("The books available in the library: " + library);
+
+// Function to add new book==========================>
+function addBook(){
+    const new_1 = '48 laws of Power', new_2 = 'Things fall Appart'
+  const addBook = console.log("We added to new books: " + library.push('48 laws of Power', 'Things fall Appart' ));   
+}
+console.log();
+
+addBook();
+console.log(library);
+
+// Function to add new book========================> 
+function delBook(){
+    const delBook = console.log(library.pop());   
+}
+delBook();
+console.log(library);
+
+// Function to get a book========================> 
+function getAnyBook(){
+    const getAnyBook = library.findIndex(library, 5);
+    console.log(getAnyBook);
+    
+
+}
+getAnyBook();
+// console.log(getAnyBook);
